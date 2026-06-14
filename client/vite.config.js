8b59454d418601f0b41811f49980c6a7dd16bd72
Om-Govindani <omgovindani@gmail.com>
2026-06-14 11:27:33 +0530

    changing the server URL in client

--- client/vite.config.js
+++ client/vite.config.js
@@ -4,11 +4,11 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react() , tailwindcss()],
+  plugins: [react(), tailwindcss()],
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://telecloud-api.vercel.app',
         changeOrigin: true,
         credentials: true
       }
